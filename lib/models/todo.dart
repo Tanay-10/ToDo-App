1742442801213bb54c632340b3e88232970a45dc
Tanay-10 <agarwaltanay2002@gmail.com>
2022-01-29 14:30:21 +0530

    some additions

--- lib/models/todo.dart
+++ lib/models/todo.dart
@@ -6,29 +6,34 @@ class ToDoModel {
   int? category_id;
   String? description;
   int? priority;
+  int? is_done;
 
-  ToDoModel(
-      {int? id,
-      String? title,
-      int? category_id,
-      int? priority,
-      String? description}) {
+  ToDoModel({
+    int? id,
+    String? title,
+    int? category_id,
+    int? priority,
+    String? description,
+    int? is_done,
+  }) {
     [
       this.id = id,
       this.category_id = category_id,
       this.priority = priority,
       this.title = title,
       this.description = description,
+      this.is_done = is_done,
     ];
   }
 
-  Map<String, dynamic>? toMap() {
+  Map<String, dynamic?> toMap() {
     return {
       "id": this.id,
       "category_id": this.category_id,
       "title": this.title,
       "priority": this.priority,
       "description": this.description,
+      "is_done": is_done,
     };
   }
 
