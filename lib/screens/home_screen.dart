be9b5ea5f48af41a0b13b6ab069cc0bfd2ea9839
Tanay-10 <agarwaltanay2002@gmail.com>
2022-01-31 20:55:58 +0530

    Changed to a stateful widget, Future builder implementation

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:todo/task.dart';
-import 'new_task_screen.dart';
 import 'routing.dart' as routing;
 import 'package:todo/database/sqlite.dart';
-import 'package:todo/task.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
@@ -23,47 +21,75 @@ class _MyHomePageState extends State<MyHomePage> {
           backgroundColor: CupertinoColors.systemBlue,
           elevation: 1.0,
           hoverElevation: 10.0,
-          child: Icon(
+          child: const Icon(
             Icons.add,
             size: 50,
           ),
-          // child: Text("Click"),
           onPressed: () {
             print("clicked");
-            Navigator.pushNamed(context, routing.newTaskScreenId);
+            Navigator.pushNamed(context, routing.newTaskScreenId,
+                arguments: Task);
           }),
       appBar: AppBar(
         backgroundColor: Colors.blue,
         title: Text("ToDo"),
       ),
-      body: FutureBuilder<List<Task>>(
-          future: taskList,
-          builder: (BuildContext context, AsyncSnapshot snapshot) {
-            if (snapshot.hasData) {
-              var data = snapshot.data;
-              List<Widget> children = [];
-              for (var task in data) {
-                children.add(ActivityCard(
-                  header: task.taskName,
-                  date: task.deadlineDate == null
-                      ? ""
-                      : task.deadlineDate.toString(),
-                  list: task.taskListId.toString(),
-                ));
-              }
-              return ListView(
-                scrollDirection: Axis.vertical,
-                children: children,
-                padding: const EdgeInsets.all(5),
-              );
-            } else if (snapshot.hasError) {
-              return const Text("Some error");
-            } else {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-          }),
+      body:
+          // Column(
+          //   children: [
+          //     Container(
+          //       child: Text("Hello"),
+          //     ),
+          FutureBuilder<List<Task>>(
+              future: taskList,
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                if (snapshot.hasData) {
+                  var data = snapshot.data;
+                  List<Widget> children = [];
+                  children.add(Container(
+                    child: const Center(
+                      child: Text(
+                        "Overdue",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: CupertinoColors.activeBlue,
+                        ),
+                      ),
+                    ),
+                  ));
+                  for (var task in data) {
+                    print("FutureBuilder");
+                    children.add(
+                      ActivityCard(
+                          header: task.taskName,
+                          date: task.deadlineDate == null
+                              ? ""
+                              : task.deadlineDate.toString(),
+                          list: task.taskListId.toString(),
+                          onTap: () {
+                            Navigator.pushNamed(
+                                context, routing.newTaskScreenId,
+                                arguments: task);
+                          }),
+                    );
+                  }
+                  return ListView(
+                    // scrollDirection: Axis.vertical,
+                    children: children,
+                    padding: const EdgeInsets.all(5),
+                  );
+                } else if (snapshot.hasError) {
+                  // print(snapshot.hasError);
+                  return const Text("Some error");
+                } else {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+              }),
+      // ],
+      // ),
       // Container(
       //   // color: Colors.blueAccent,
       //   padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
@@ -94,86 +120,80 @@ class _MyHomePageState extends State<MyHomePage> {
 
 class ActivityCard extends StatelessWidget {
   final String header, date, list;
+  final void Function() onTap;
 
   const ActivityCard({
     required this.header,
     required this.date,
     required this.list,
+    required this.onTap,
     Key? key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {
-        Navigator.pushNamed(context, routing.newTaskScreenId);
-      },
-      child: Column(
-        children: [
-          Card(
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(7.0)),
-            child: Container(
-              width: double.maxFinite,
-              padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-              child: Row(
+      onTap: onTap,
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(7.0),
+        ),
+        child: Container(
+          width: double.maxFinite,
+          padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                width: 20,
+                height: 25,
+                child: Checkbox(
+                  value: false,
+                  onChanged: (value) {
+                    print("task finished");
+                  },
+                  autofocus: true,
+                ),
+              ),
+              const SizedBox(
+                width: 12,
+              ),
+              Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Container(
-                    width: 20,
-                    height: 25,
-                    child: Checkbox(
-                      value: false,
-                      onChanged: (value) {
-                        print("task finished");
-                      },
-                      autofocus: true,
+                  Text(
+                    header,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w500,
                     ),
                   ),
                   const SizedBox(
-                    width: 12,
+                    height: 5,
                   ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        header,
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 5,
-                      ),
-                      Text(
-                        date,
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 17.0,
-                          fontWeight: FontWeight.w300,
-                        ),
-                      ),
-                      Text(
-                        list,
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 16.0,
-                          fontWeight: FontWeight.w300,
-                        ),
-                      ),
-                    ],
+                  Text(
+                    date,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 17.0,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                  Text(
+                    list,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.w300,
+                    ),
                   ),
                 ],
               ),
-            ),
-            color: Colors.teal,
+            ],
           ),
-          const SizedBox(
-            height: 6,
-          )
-        ],
+        ),
+        color: Colors.teal,
       ),
     );
   }
