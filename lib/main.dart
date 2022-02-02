981a903307e80a95130ea81f99e370740190798a
Tanay-10 <agarwaltanay2002@gmail.com>
2022-02-02 18:24:41 +0530

    Change notifier provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'database/sqlite.dart';
 import 'screens/new_task_screen.dart';
 import 'screens/routing.dart' as routing;
 import 'screens/home_screen.dart';
-import 'task.dart';
+import 'package:todo/task.dart';
+import 'package:todo/shared_data.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  SqliteDB.initDb();
+  await SqliteDB.initDb();
   runApp(const MyApp());
 }
 
@@ -17,32 +19,35 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(),
-      // routes: {
-      //   routing.newTaskScreenId: (context) => NewTaskScreen(),
-      //   routing.homeScreenId: (context) => const MyHomePage(),
-      // },
-      onGenerateRoute: (settings) {
-        var pageName = settings.name;
-        var args = settings.arguments;
-        if (pageName == routing.newTaskScreenId) {
-          if (args is Task) {
-            return MaterialPageRoute(
-                builder: (context) => NewTaskScreen(
-                      task: args,
-                    ));
+    return ChangeNotifierProvider(
+      create: (context) => ToDoData(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const MyHomePage(),
+        // routes: {
+        //   routing.newTaskScreenId: (context) => NewTaskScreen(),
+        //   routing.homeScreenId: (context) => const MyHomePage(),
+        // },
+        onGenerateRoute: (settings) {
+          var pageName = settings.name;
+          var args = settings.arguments;
+          if (pageName == routing.newTaskScreenId) {
+            if (args is Task) {
+              return MaterialPageRoute(
+                  builder: (context) => NewTaskScreen(
+                        task: args,
+                      ));
+            }
+            return MaterialPageRoute(builder: (context) => NewTaskScreen());
           }
-          return MaterialPageRoute(builder: (context) => NewTaskScreen());
-        }
-        if (pageName == routing.homeScreenId) {
-          return MaterialPageRoute(builder: (context) => const MyHomePage());
-        }
-      },
+          if (pageName == routing.homeScreenId) {
+            return MaterialPageRoute(builder: (context) => const MyHomePage());
+          }
+        },
+      ),
     );
   }
 }
