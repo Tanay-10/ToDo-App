06923151071ee7e7539c352b9bdc9f31c74ebd16
Tanay-10 <agarwaltanay2002@gmail.com>
2022-02-03 14:32:50 +0530

    CRUD operations on task

--- lib/shared_data.dart
+++ lib/shared_data.dart
@@ -1,31 +1,28 @@
+import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:todo/task.dart';
 import 'database/sqlite.dart';
 
-class SharedData extends ChangeNotifier {
-  int x = 1, y = 2;
-  SharedData();
-
-  incrementX() {
-    x = x + 1;
-    notifyListeners();
-  }
-}
-
 class ToDoData extends ChangeNotifier {
   bool isDataLoaded = false;
-  List<Task> activeTaskList = [];
+  List<Task> activeTasks = [];
+
   ToDoData() {
     init();
   }
+
+  List<TaskList> activeLists = [];
+
   void init() async {
-    activeTaskList = await SqliteDB.getAllPendingTasks();
+    activeTasks = await SqliteDB.getAllPendingTasks();
+    activeLists = await SqliteDB.getAllActiveLists();
     isDataLoaded = true;
     notifyListeners();
   }
 
   void addTask(Task task) async {
+    print("I am add task");
     var taskAsMap = task.toMap();
     taskAsMap.remove("taskId");
     int? id = await SqliteDB.insertTask(taskAsMap);
@@ -33,8 +30,63 @@ class ToDoData extends ChangeNotifier {
       print("could not insert");
     } else {
       task.taskId = id;
-      activeTaskList.add(task);
+      activeTasks.add(task);
       notifyListeners();
     }
+  }
+
+  void updateTask(Task task) async {
+    print("I am update task");
+    bool success = await SqliteDB.updateTask(task);
+    if (success == false) {
+      print("could not update");
+    }
+    {
+      var index = findTaskIndex(task);
+      if (index != null) {
+        activeTasks[index] = task;
+      } else {
+        print("task not found");
+      }
+    }
+    notifyListeners();
+  }
+
+  void deleteTask(Task task) async {
+    bool success = await SqliteDB.deleteTask(task);
+    if (success == false) {
+      print("could not delete task");
+    } else {
+      var index = findTaskIndex(task);
+      if (index != null) {
+        activeTasks.removeAt(index);
+        notifyListeners();
+        print("Task with id ${activeTasks[index].taskId} deleted");
+      } else {
+        print("task not found");
+      }
+    }
+  }
+
+  void finishTask(Task task) async {
+    task.isFinished = true;
+    SqliteDB.updateTask(task);
+    var index = findTaskIndex(task);
+    if (index == null) {
+      print("Task not found");
+    } else {
+      activeTasks.removeAt(index);
+      notifyListeners();
+    }
+  }
+
+  int? findTaskIndex(Task task) {
+    var index = activeTasks.indexWhere((Task t) {
+      return t.taskId == task.taskId;
+    });
+    if (index == -1) {
+      return null;
+    }
+    return index;
   }
 }
