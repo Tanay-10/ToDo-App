f4a2bb9c2713ffae64a91b75338351febe7fdb36
Tanay-10 <agarwaltanay2002@gmail.com>
2022-02-10 23:59:31 +0530

    splash screen builder, widget building implementation

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -15,38 +15,30 @@ class SplashScreen extends StatefulWidget {
   _SplashScreenState createState() => _SplashScreenState();
 }
 
-Future<int> initialization() async {
-  await Firebase.initializeApp();
-  await SqliteDB.initDb();
-  // await Future.delayed(Duration(seconds: 10));
-  return 1;
-}
-
 class _SplashScreenState extends State<SplashScreen> {
-  Future<int> init = initialization();
+  @override
+  void initState() {
+    // TODO: implement initState
+    // super.initState();
+    WidgetsBinding.instance!.addPostFrameCallback((timeStamp) async {
+      await Firebase.initializeApp();
+      await SqliteDB.initDb();
+      FirebaseAuth auth = FirebaseAuth.instance;
+      if (auth.currentUser != null) {
+        Provider.of<ToDoData>(context, listen: false).init();
+        Navigator.pushNamedAndRemoveUntil(
+            context, routing.homeScreenId, (route) => false);
+      } else {
+        Navigator.pushNamedAndRemoveUntil(
+            context, routing.socialSignInId, (route) => false);
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<int>(
-      future: init,
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          FirebaseAuth auth = FirebaseAuth.instance;
-          if (auth.currentUser == null) {
-            Provider.of<ToDoData>(context, listen: false).init();
-            return SocialSignIn();
-          } else {
-            return MyHomePage();
-          }
-        } else if (snapshot.hasError) {
-          return Center(
-            child: Text("Error"),
-          );
-        } else
-          return Center(
-            child: CircularProgressIndicator(),
-          );
-      },
+    return Center(
+      child: Text("Welcome"),
     );
   }
 }
