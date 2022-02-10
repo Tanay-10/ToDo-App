19c0c0b47424f7296cbdd30d131c6b1d9dcb4886
Tanay-10 <agarwaltanay2002@gmail.com>
2022-02-10 23:57:25 +0530

    widget building initialization moved

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,6 @@ import 'screens/splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
